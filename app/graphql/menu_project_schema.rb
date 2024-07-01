41d68ca1645c0681f2a39fd766e4c6b4563cc33d
Kenny Hwu <kennywhwu@gmail.com>
2024-07-01 21:33:09 +0800

    Update schema name in controller

--- app/graphql/menu_project_schema.rb
+++ app/graphql/menu_project_schema.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
-class GraphqlMenuProjectSchema < GraphQL::Schema
+class MenuProjectSchema < GraphQL::Schema
+  mutation(Types::MutationType)
   query(Types::QueryType)
+
+  # Relay-style Object Identification:
+
+  # Return a string UUID for `object`
+  def self.id_from_object(object, type_definition, query_ctx)
+    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
+    object.to_gid_param
+  end
+
+  # Given a string UUID, find the object
+  def self.object_from_id(global_id, query_ctx)
+    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
+    GlobalID.find(global_id)
+  end
 end
