1e5116b4cffa92144f6a3e509e9811c1e11e8237
Kenny Hwu <kennywhwu@gmail.com>
2024-07-01 22:16:34 +0800

    Fix Types typo

--- app/graphql/types/menu_section_type.rb
+++ app/graphql/types/menu_section_type.rb
@@ -3,8 +3,8 @@
 module Types
   class MenuSectionType < Types::BaseObject
     field :id, ID, null: false
-    field :menus, [Type::MenuType], null: false
-    field :sections, [Type::SectionType], null: false
+    field :menus, [Types::MenuType], null: false
+    field :sections, [Types::SectionType], null: false
     field :display_order, Integer, default: 0
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
